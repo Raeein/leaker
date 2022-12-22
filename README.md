c9c2da453d2f280d9c85e7cfe60ebe446ebf09ba
Raeein <raeeinbagheri80@gmail.com>
2022-12-22 16:07:40 -0500

    Add command line arguments

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # leaker
-# leaker
--- entry.sh
+++ entry.sh
@@ -2,56 +2,58 @@
 
 Help()
 {
-   echo "Compile and run Valgrind on school server(kinda). Run this container from the root of your C project."
+   echo "Compile and run Valgrind on school server (kinda). Run this container from the root of your C project."
    echo
    echo "Syntax:"
-   echo "docker run -v $(pwd):/root/build --rm --name leaker raeein/leaker:test main_path"
+   echo "docker run -v \$(pwd):/root/build --rm --name leaker raeein/leaker:latest -m main_path -v \"args\""
    echo
    echo "arguments:"
-   echo "main_path     Relative path from root of the project to the C file that has the main function"
+   echo "-m     Relative path from root of the project to the C file that has the main function."
+   echo "-v     Arguments required to run the executable. Put all the args in a \" \" or "
+   echo "       add them with -v flags. ex: -v arg1 -v arg2."
+   echo "-h     Help Menu."
    echo
 }
 
-while getopts ":h" option; do
-   case $option in
-      h)
-         Help
-         exit;;
+
+while getopts "m:v:h" opt; do
+   case $opt in
+      m) MAIN="$OPTARG";;
+      v) ARGS+="$OPTARG ";;
+      h) Help; exit;;
+      *) echo "Unknown";;
    esac
 done
+shift $((OPTIND -1))
+
+if [ -z "$MAIN" ]
+  then
+    echo 
+    echo "No main file was give. Run with -h for more infomation"
+    exit 1
+fi
+
+
+echo "Main: $MAIN"
+echo "Args: $ARGS"
 
 
 EXEC="leaky"
 
-if [ $# -le 1 ]
-  then
-    echo "Supply the correct number of argument. Run with -h for more infomation."
-    echo "$0 /path/to/main.c [args]"
-    exit 1
-fi
-
-while getopts "v:" opt; do
-    case $opt in
-        v) multi+=("$OPTARG");;
-        #...
-    esac
-done
-shift $((OPTIND -1))
-
 command -v valgrind >/dev/null 2>&1 || { echo >&2 "Valgrind is not installed.  Aborting."; exit 1; }
 
-echo "Path to main: $1"
 echo "Compiling the binary..."
 
-gcc -Wall -std=c99 -ggdb3 -o $EXEC $1
+gcc -Wall -std=c99 -ggdb3 -o "$EXEC" "$MAIN"
 COMPILED=$?
 
 if [ $COMPILED -ne 0 ]; then
-    echo "Failed to compile. $COMPILED"
+    echo "Failed to compile."
+    exit 1
 fi
 
-echo 
 echo "Running valgrind..."
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all $EXEC
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./"$EXEC" "$ARGS"
 
+echo "Cleaning up..."
 rm $EXEC
